cf6f933be0d728426d26146b7ab33ecc1c003cb5
daizuyaro <68910511+daizuyaro@users.noreply.github.com>
2022-08-18 08:38:28 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,234 +1,223 @@
-# python-3.7.6-amd64
+# https://imagingsolution.net/program/python/tkinter/display_opencv_video_canvas/
 
-import sys
-from PySide6.QtWidgets import *
-from PySide6.QtCore import *
+import pickle
+import tkinter as tk
+from tkinter import messagebox
+#import stapipy as st
+import cv2 as cv
+from PIL import Image, ImageTk # Pillow
+import stapipy as st
+import camera
+import judge
+#import node_value_change
+#import mouse_event
 from multiprocessing import Process, Queue, pool
-import csv
-import os
-import configparser
-import pyautogui as pag
-
-#DWM Serial Parser by Brian H. | Updated 3/6/19
-import serial
 import time
-import datetime
-from scipy.spatial import distance
 import numpy as np
 from datetime import datetime
+import trigger
+import dill
+import pickle
+import image_convert
+import signal
+import threading
+import gc
+import tkinter as tk
+import pyautogui  # 外部ライブラリ
+from PIL import Image, ImageTk  # 外部ライブラリ
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
 
-import tag_position
-import dwm1001
-#import uart_shell_commands
 
-#config.ini
-#parser = configparser.SafeConfigParser() # for ini file
-#parser.read("config.ini") # deployment for ini fil
-#sleep_loop = parser.get("sleep", "loop")
-#sleep_alarm = parser.get("sleep", "alarm") # threshold value
-#sleep_logger = parser.get("sleep", "logger")
-#name00 = parser.get("name", "name00") # worker name
-#name01 = parser.get("name", "name01") # worker name
-#name02 = parser.get("name", "name02") # worker name
-#filepath_txt ="data\\管理番号.txt" #filepath for "管理番号": tempolarly path, not important
+# Image scale when displaying using OpenCV.
+DISPLAY_RESIZE_FACTOR = 0.3
+# imposible to set infinity as -1
+i = 999999999999999
+PIXEL_FORMAT = "UserOutputValue0"
+RESIZE_RETIO = 2 # 縮小倍率の規定
+canvas_width = 500
+canvas_height = 310
 
-# while loop, data from MITUTOYO degimatic guage
-def loop(q): #OK
-    i = []
-    #DWM=serial.Serial(port="COM6", baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=0.1, xonxoff=True, rtscts=True, dsrdtr=True) # maximum update rate = 10Hz (0.1s)
-    s = dwm1001.shell('COM6') #change this number to suit different setup
-    s.dEnter()
+
+def main(q, r):
+    # camera initialization
+    st.initialize()
+    st_system = st.create_system()
+    st_device = st_system.create_first_device()
+    st_datastream = st_device.create_datastream()
+    st_datastream.start_acquisition(i)
+    st_device.acquisition_start()
+    remote_nodemap = st_device.remote_port.nodemap # impossible to pick it, so combined into the main program
+
+    while st_datastream.is_grabbing: # A while loop for acquiring data and checking status
+        data = camera.loop(st_datastream) # data from the camera.py
+        q.put(data) # for image
+        sim_fact = judge.judgement(data) # template matching and similarity factor
+        if sim_fact > 0.9:
+            ok = "OK"
+            r.put(ok) # for image
+
+        else: #
+            ng = "NG"
+            r.put(ng) # for image
+            trigger.edit_enumeration(remote_nodemap, PIXEL_FORMAT, 1)
+            # メッセージボックス（情報）
+            #tk.messagebox.showinfo('確認', 'もうすぐ誕生日です')
+            trigger.edit_enumeration(remote_nodemap, PIXEL_FORMAT, 0)
+            print("NG")
+
+    # convert the image from nupy to image for the TK
+def converter(q, s):
     while True:
-        s.loop('les')
-    s.close()
-    print(s)
+        image_tk = image_convert.converter(q.get())
+        s.put(image_tk)
 
-    #q.put(value)
-    #print(value)
-
-# data logger from MITUTOYO device
-def logger(q):
-
-    filepath_csv = "D:\\Desktop\\a.txt"
-
-    # read 管理番号 from the txt file
-    #f = open(filepath_txt, "r")
-    #filepath_csv = f.readline()
-
-    # csv file
+def save(q):
     while True:
-        value = q.get()
-        f = open(filepath_csv, "a")
-        writer =csv.writer(f, lineterminator='\n')
-        writer.writerow([value])
-        f.close()
+        cv.imwrite('D:\\Programing\\PycharmProjects\\image_processing\\data\\a.jpg', q.get(), [cv.IMWRITE_JPEG_QUALITY, 100])
 
-#necessary file for multiprocessing
-class Consumer(QThread):
+class Mainloop(tk.Frame):
+    def __init__(self, r, master=None):
+        super().__init__(master)
+        self.pack()
 
-    poped = Signal(str)
+        #loop = asyncio.new_event_loop()
+        #asyncio.set_event_loop(loop)
 
-    def __init__(self, q):
-        super().__init__()
-        self.q = q
+        button = tk.Button(text='ボタン', width=30, command=self.thread)
+        button.place(x=5, y=400)
+        button.bind('<Button-1>', )
 
-    def run(self):
-        while True:
-            self.poped.emit(q.get())
+        #フレームを入れ子で作成。「frame」の中に「canvas」を作成
+        frame = tk.Frame(root, relief=tk.FLAT)
+        frame.place(x=5, y=5, width=canvas_width, height=canvas_height)
+        ### キャンバス作成・配置
+        self.canvas = tk.Canvas(frame, width=canvas_width, height=canvas_height)
+        self.canvas.place(x=5, y=5)
+        self.canvas1 = tk.Canvas(frame, width=canvas_width, height=canvas_height)
+        self.canvas1.place(x=530, y=5)
 
-# GUI
-class MyWindow(QMainWindow):
-
-    def __init__(self, q):
-        super().__init__()
-
-        self.wo01 = QLineEdit("", self)
-        self.wo01.move(20,10)
-        self.wo01.resize(400*0.8, 80)
-        self.wo01.setPlaceholderText(' 作業用番号') # 透かしで表示
-        self.wo01.setStyleSheet("color: black; font: 20pt Arial; border-color: black; "
-                                "border-style:solid; border-width: 1.5px; border-radius:10px") # 文字の大きさなど
-
-
-        self.wk01 = QComboBox(self)
-        #self.wk01.addItem(name00)
-        self.wk01.move(390,10)
-        self.wk01.resize(400*0.8, 80)
-        self.wk01.setStyleSheet("color: black; font: 20pt Arial; border-color: white; "
-                                "border-style:solid; border-width: 1.5px; border-radius:10px") # 文字の大きさなど
-
-        self.btn01 = QPushButton("計測開始", self)
-        self.btn01.move(750, 10)
-        self.btn01.resize(400*0.8, 80)
-        self.btn01.setStyleSheet("color: black; font: 20pt Arial; border-color: black; "
-                                 "border-style:solid; border-width: 1.5px; border-radius:10px") # 文字の大きさなど
-        self.btn01.clicked.connect(self.header)
-
-        self.setGeometry(200, 200, 300, 200)
-
-        self.label = QLabel(self)
-        self.label.move(0, 100)
-        self.label.resize(1366, 400)
-
-        HBox = QHBoxLayout()
-        HBox.addWidget(self.wo01)
-        HBox.addWidget(self.wk01)
-        HBox.addWidget(self.btn01)
-        HBox.addWidget(self.label)
-        HBox.addStretch(0)
-
-    # header for CSV + logger start
-    def header(self):
-        # csv header
-        self.filepath_csv = filepath_csv + self.wo01.text() + ".csv"
-
-        # to change the file name if the self.wo01.text() is in blank
-        if "\.csv" in self.filepath_csv:
-            dt = datetime.datetime.now()
-            dt1 = dt.strftime('%Y%m%d')
-            dt2 = dt.strftime('%H%M%S')
-            self.filepath_csv =str(self.filepath_csv).replace('.csv', '')
-            filename = str(dt1) + str(dt2) + ".csv"
-            self.filepath_csv = filepath_csv + filename
-
-        else:
-            pass
-
-        # check the file has been already existed or not, if not save headers.
-        if not os.path.exists(self.filepath_csv):
-            # write the header
-            csvlist = []
-            f = open(self.filepath_csv, 'w')
-            writer = csv.writer(f, lineterminator='\n')
-
-            writer.writerow(["作業番号", self.wo01.text()])
-            writer.writerow(["作業者名", self.wk01.currentText()])
-            writer.writerow(["日付","時間","熱変形量[mm]","闘値超え"])
-
-            writer.writerow(csvlist)
-            f.close()
-
-        else:
-            pass
-
-        # to pass the 作業用番号 to the "def logger": saved in txt file
-        with open(filepath_txt, mode="w") as f:
-            f.write(self.filepath_csv)
-
-        # thread for data consumer
-        self.consumer = Consumer(q)
-        self.consumer.poped.connect(self.print_data)
-        self.consumer.start()
-
-        # multiprocessing p2 start
-        p2.start()
+        self.text= tk.StringVar()
+        self.text.set("aaaa")
+        self.text_pos = tk.Label(root, textvariable=self.text)
+        self.text_pos.place(x=5, y=5)
 
     #@pyqtSlot(str)
-    def print_data(self, data):
-        #self.statusBar().showMessage(data)
+    def canvas_update(self, r, s):
+        increment_OK = 0
+        increment_NG = 0
 
-        #print(type(data))
-        #if 0 <= float(data) < 0.045:
-        #self.label.setText("  " + data)
-        #self.styleA = "QWidget{background-color:%s; font: 300pt Arial}" % ("green")
-        #self.label.setStyleSheet(self.styleA)
+        # Canvasウィジェットを配置し、各種イベントを設定
+        self.canvas.pack()
+        self.canvas.bind("<ButtonPress-1>", self.start_point_get)
+        self.canvas.bind("<Button1-Motion>", self.rect_drawing)
+        self.canvas.bind("<ButtonRelease-1>", self.release_action)
 
-        #elif 0.045 <= float(data) <= 0.049:
-        #self.label.setText("  " + data)
-        #self.styleA = "QWidget{background-color:%s; font: 300pt Arial}" % ("yellow")
-        #self.label.setStyleSheet(self.styleA)
+        print(self.start_x, self.start_y, self.end_x, self.end_y)
+        self.canvas.create_rectangle(self.start_x, self.start_y, self.end_x, self.end_y, outline='white')
 
-        #elif 0.050 <= float(data) < 100:
-        #self.label.setText("  " + data)
-        #self.styleA = "QWidget{background-color:%s; font: 300pt Arial}" % ("brown")
-        #self.label.setStyleSheet(self.styleA)
+        self.image_tk = ImageTk.PhotoImage(s.get())
+        # 画像の描画
+        self.canvas.create_image(
+            #self.canvas_width / 2,       # 画像表示位置(Canvasの中心)
+            #self.canvas_height / 2,
+            0, 0, image=self.image_tk, anchor='nw')
+
+        if r.get() == "OK":
+            increment_OK += 1
+            self.text.set("OK" + " " + str(increment_OK))
+        else:
+            increment_NG += 1
+            self.text.set("NG" + " " + str(increment_NG))
+
+    # ドラッグ開始した時のイベント - - - - - - - - - - - - - - - - - - - - - - - - - -
+    def start_point_get(self, event):
+        global start_x, start_y # グローバル変数に書き込みを行なうため宣言
+
+        self.canvas.delete("rect1")  # すでに"rect1"タグの図形があれば削除
+
+        # canvas上に四角形を描画（rectangleは矩形の意味）
+        self.canvas.create_rectangle(event.x,
+                                 event.y,
+                                 event.x + 1,
+                                 event.y + 1,
+                                 outline="white",
+                                 tag="rect1")
+        # グローバル変数に座標を格納
+        start_x, start_y = event.x, event.y
+
+    # ドラッグ中のイベント - - - - - - - - - - - - - - - - - - - - - - - - - -
+    def rect_drawing(self, event):
+
+        # ドラッグ中のマウスポインタが領域外に出た時の処理
+        if event.x < 0:
+            end_x = 0
+        else:
+            end_x = min(canvas_width, event.x)
+        if event.y < 0:
+            end_y = 0
+        else:
+            end_y = min(canvas_height, event.y)
+
+        # "rect1"タグの画像を再描画
+        self.canvas.coords("rect1", start_x, start_y, end_x, end_y)
+
+    # ドラッグを離したときのイベント - - - - - - - - - - - - - - - - - - - - - - - - - -
+    def release_action(self, event):
+
+        # "rect1"タグの画像の座標を元の縮尺に戻して取得
+        self.start_x, self.start_y, self.end_x, self.end_y = [
+            round(n * RESIZE_RETIO) for n in self.canvas.coords("rect1")
+        ]
+
+        # 取得した座標の部分を切り出し
+        coordination = pyautogui.confirm(text="検索範囲に指定しますか?", title="検索範囲", buttons=["Yes","No"])
+
+        if coordination == "Yes":
+
+            # 画像の描画
+            im = Image.open('D:\\Programing\\PycharmProjects\\image_processing\\data\\a.jpg')
+            im_crop = im.crop((self.start_x, self.start_y, self.end_x, self.end_y))
+            im_crop.save('D:\\Programing\\PycharmProjects\\image_processing\\data\\b.jpg', quality=100)
+
+            image_tk = image_convert.converter(im_crop)
+            image_tk = ImageTk.PhotoImage(image_tk)
+            self.canvas1.create_image(
+                #self.canvas_width / 2,       # 画像表示位置(Canvasの中心)
+                #self.canvas_height / 2,
+                0, 0, image=image_tk)
 
 
 
-        if -100 <= float(data) < 0:
-            self.label.setText(" " + data)
-            self.styleA = "QWidget{font: 250pt}"
-            self.label.setStyleSheet(self.styleA)
+        else:
+            self.canvas.delete("rect1")  # すでに"rect1"タグの図形があれば削除
+            pass
 
-        if 0 <= float(data) < 10:
-            self.label.setText("  " + data)
-            self.styleA = "QWidget{font: 250pt}"
-            self.label.setStyleSheet(self.styleA)
-
-        if 10 <= float(data) < 100:
-            self.label.setText(" " + data)
-            self.styleA = "QWidget{font: 250pt}"
-            self.label.setStyleSheet(self.styleA)
-
-        elif float(data) == -100:
-            data = " 異常発生"
-            self.label.setText(data)
-            self.styleA = "QWidget{color:%s; font: 180pt Arial}" % ("brown")
-            self.label.setStyleSheet(self.styleA)
+    def thread(self):
+        thread = threading.Thread(target=self.canvas_update, args=(r, s, ))
+        thread.start()
 
 
 if __name__ == "__main__":
 
     q = Queue()
+    r = Queue()
+    s = Queue()
 
     with pool.Pool(processes=3) as p:
 
         #multiprocessing
-        p0 = p.Process(target=loop, args=(q, ), daemon=True)
-        #p1 = p.Process(target=alarm, args=(q, ), daemon=True)
-        p1 = p.Process(target=logger, args=(q, ), daemon=True)
+        p0 = p.Process(target=main, args=(q, r, ), daemon=True)
+        p1 = p.Process(target=converter, args=(q, s, ), daemon=True)
+        p2 = p.Process(target=save, args=(q, ), daemon=True)
 
         p0.start()
         p1.start()
+        p2.start()
 
-    # Main process
-    app = QApplication(sys.argv)
-    mywindow = MyWindow(q)
-    mywindow.setWindowTitle("熱変形量監視ソフト Version 0.0.0") # windowのtitle
-    #mywindow.showFullScreen()
+    root = tk.Tk()
+    root.title("Hello World")
+    root.geometry("1024x768")
+    app = Mainloop(r, master = root)
+    app.mainloop()
 
-    scr_w,scr_h= pag.size() #LCD resolution
-
-    mywindow.setFixedSize(scr_w*0.8, scr_h*0.8)  # windows size based on LCD resolution
-    mywindow.show()
-    app.exec()
